b69cd767a9c8c9c8d338138b373b057d50eddefb
4esnog <4esnog96@gmail.com>
2016-08-07 14:21:51 +0300

    Use andre487/infra-example

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-// app.use(express.static('public'));
+const express = require('express');
+const getFace = require('cool-ascii-faces');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    console.log('GET');
-    console.log(''+__dirname);
-    res.send('Test!');
+    console.time('render');
+    res.render('index', { face: getFace() });
+    console.timeEnd('render');
 });
 
-app.listen(3000, function () {
-    console.log('My example');
+app.listen(app.get('port'), function () {
+    console.log('Cool faces on port', app.get('port'));
 });
